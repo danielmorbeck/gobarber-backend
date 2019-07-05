9476b8bd798c455fbed867345e30dcfb81c8fd06
Daniel Morbeck <danielmorbeck@gmail.com>
2019-07-05 14:54:41 -0300

    Nodemailer configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '1fd7a16a52e549',
+    pass: 'a47f7a8bf775a2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
